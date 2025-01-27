9ec89a44da9bf0c17198a43af6629530529e4c8a
Voice Cheeser <amraniaybb@gmail.com>
2025-01-27 03:49:30 +0100

    third

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <div>hello</div>;
+  return (
+    <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <div className="container">
+        <h1>Quiz App</h1>
+        <a href="/quiz">
+          <button>Start Quiz</button>
+        </a>
+      </div>
+    </main>
+  );
 }
---
+++ app/quiz/page.tsx
@@ -0,0 +1,44 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+"use client";
+import React, { useState } from "react";
+import { quiz } from "./data";
+
+const Page = () => {
+  const [activeQuestion, setActiveQuestion] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState("");
+  const [checked, setChecked] = useState(false);
+  const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null);
+  const [showResult, setShowResult] = useState(false);
+  const [result, setResult] = useState({
+    score: 0,
+    correctAnswers: 0,
+    wrongAnswers: 0,
+  });
+
+  const { questions } = quiz;
+  const { question, answers, correctAnswer } = questions[activeQuestion];
+
+  return (
+    <div className="container">
+      <h1>Quiz Page</h1>
+      <div>
+        <h2>
+          Question: {activeQuestion + 1}
+          <span>/{questions.length}</span>
+        </h2>
+      </div>
+      <div>
+        {!showResult ? (
+          <div className="quiz-container">
+            <h3>{questions[activeQuestion].question}</h3>
+          </div>
+        ) : (
+          <div className="quiz-container"></div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Page;
